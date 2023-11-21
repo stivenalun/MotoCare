ebd91c80588d562b5258a6fa2eb0062bd2440b05
nurhidayatulf <nurhidayatulfatihah19@gmail.com>
2023-11-21 18:32:06 +0700

    update riwayat page

--- MotoCare/View/Maintenance History/MaintenanceHistoryView.swift
+++ MotoCare/View/Maintenance History/MaintenanceHistoryView.swift
@@ -10,34 +10,37 @@ import SwiftData
 
 struct MaintenanceHistoryView: View {
     
-   @Query(sort: \MaintenanceHistory.date, order: .reverse) var maintenanceHistories: [MaintenanceHistory]
-   @Query var sparepartHistories: [SparepartHistory]
-   
-   var body: some View {
-       NavigationView {
-           ZStack {
-               BackgroundView()
-               VStack {
-                  List(maintenanceHistories) { history in
-                      VStack(alignment: .leading) {
-                          Spacer()
-                          HStack {
-                              Text("\(history.date.formatted(.dateTime.day().month().year()))")
-                              Spacer()
-                              Text("\(history.sparePartHistory.count) spare part diganti")
-                                  .fontWeight(.semibold)
-                          }
-                          Spacer()
-                          Text("Maintenance Mileage: \(history.maintenanceMileage)")
-                          Spacer()
-                      }
-                  }
-//                  .scrollContentBackground(.hidden)
-               }
-           }
-           .navigationBarTitle("Riwayat")
-       }
-   }
+    @Query(sort: \MaintenanceHistory.date, order: .reverse) var maintenanceHistories: [MaintenanceHistory]
+    //   @Query var sparepartHistories: [SparepartHistory]
+    @Query var sparepartHistories: [SparepartHistory]
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                BackgroundView()
+                VStack {
+                    List(maintenanceHistories) { history in
+                        VStack(alignment: .leading) {
+                            Spacer()
+                            HStack {
+                                Text("\(history.date.formatted(.dateTime.day().month().year()))")
+                                Spacer()
+                                Text("\(history.sparePartHistory.count) spare part diganti")
+                                    .fontWeight(.semibold)
+                            }
+                            Spacer()
+                            HStack {
+                                let sparePartNames = history.sparePartHistory.map { $0.name }.joined(separator: ", ")
+                                Text(sparePartNames)
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Riwayat")
+        }
+    }
 }
 
 #Preview {
