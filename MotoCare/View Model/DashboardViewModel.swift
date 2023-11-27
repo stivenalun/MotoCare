a48338c43aa42d9b67267507a710c1ab13cff229
stivenalun <stiven23@ilb.idserve.net>
2023-11-27 14:43:10 +0700

    DashboardViewModel

--- MotoCare/View Model/DashboardViewModel.swift
+++ MotoCare/View Model/DashboardViewModel.swift
@@ -2,14 +2,12 @@
 //  DashboardViewModel.swift
 //  MotoCare
 //
-//  Created by Nur Hidayatul Fatihah on 25/11/23.
+//  Created by Stiven on 27/11/23.
 //
 
 import Foundation
-import SwiftUI
+
 
 class DashboardViewModel: ObservableObject {
     @Published var bluetoothService = BluetoothService()
 }
-
-//test y
